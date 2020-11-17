647c3a933b44aca0971e3decc24ff07c715ab558
CristinaVidal <vidal.cristina14@gmail.com>
2020-11-17 22:08:16 +0100

    Fix styles

--- Complete Vue.js 2 Web Development/12_slots/main.js
+++ Complete Vue.js 2 Web Development/12_slots/main.js
@@ -1,5 +1,5 @@
 Vue.component('modal-window', {
-	template: `<div class="modal fade" v-show="visible">
+	template: `<div class="modal fade" :class="{ 'block': visible }">
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
 					<div class="modal-header"></div>
