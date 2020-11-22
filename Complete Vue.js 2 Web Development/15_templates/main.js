ce4f8ec36be960c7fc7e6c8584eea319ecd3c423
CristinaVidal <vidal.cristina14@gmail.com>
2020-11-22 14:51:48 +0100

    In process

---
+++ Complete Vue.js 2 Web Development/15_templates/main.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+	el: '#app',
+	data: {
+		people: [{
+			name: 'Perónica',
+			age: 27
+		}, {
+			name: 'Sara sin h',
+			age: 14
+		}, {
+			name: 'Jose',
+			age: 76
+		}]
+	}
+});
