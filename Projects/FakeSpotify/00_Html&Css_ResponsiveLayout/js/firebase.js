d0e079ffc1376fa08888a6093a78584955c6a5c9
CristinaVidal <vidal.cristina14@gmail.com>
2020-12-06 12:07:30 +0100

    Testing firebase

---
+++ Projects/FakeSpotify/00_Html&Css_ResponsiveLayout/js/firebase.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyDNk79nwu9NtDbKSBRjfgmQXFnkpj9owfk",
+	authDomain: "test-uocify.firebaseapp.com",
+	projectId: "test-uocify",
+	storageBucket: "test-uocify.appspot.com",
+	messagingSenderId: "765296101164",
+	appId: "1:765296101164:web:fd2241bb31b939dcd70e89"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase);
+
+
+
+
